a765304066e914000e9e092c8604ba460985919b
blastomaniac <143513561+Bas-jpg@users.noreply.github.com>
2026-01-12 17:06:05 +0100

    added and fixed OpenGL

--- C++ first repo/C++ first repo/C++ first repo.cpp
+++ C++ first repo/C++ first repo/C++ first repo.cpp
@@ -1,23 +1,62 @@
-﻿#include <SFML/Graphics.hpp>
+﻿//#include <SFML/Graphics.hpp>
+//
+//int main()
+//{
+//    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
+//
+//    while (window.isOpen())
+//    {
+//        while (const std::optional event = window.pollEvent())
+//        {
+//            if (event->is<sf::Event::Closed>())
+//                window.close();
+//        }
+//
+//        window.clear();
+//        window.draw(shape);
+//        window.display();
+//    }
+//}
 
-int main()
+//// comentaar om te testen of dingen gecommit worden vanout github desktop
+
+
+#include <glfw3.h>
+
+int main(void)
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    GLFWwindow* window;
 
-    while (window.isOpen())
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
     {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+        glfwTerminate();
+        return -1;
     }
-}
 
-// comentaar om te testen of dingen gecommit worden vanout github desktop
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
