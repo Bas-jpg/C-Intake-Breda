21e09203a8b2ee15be800c3a5ca6f80d6f61f04d
blastomaniac <143513561+Bas-jpg@users.noreply.github.com>
2026-01-14 11:31:32 +0100

    Added the powerpoint template
    SFML libraries broke

--- C++ first repo/C++ first repo/C++ first repo.cpp
+++ C++ first repo/C++ first repo/C++ first repo.cpp
@@ -1,62 +1,62 @@
-﻿//#include <SFML/Graphics.hpp>
-//
-//int main()
-//{
-//    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-//    sf::CircleShape shape(100.f);
-//    shape.setFillColor(sf::Color::Green);
-//
-//    while (window.isOpen())
-//    {
-//        while (const std::optional event = window.pollEvent())
-//        {
-//            if (event->is<sf::Event::Closed>())
-//                window.close();
-//        }
-//
-//        window.clear();
-//        window.draw(shape);
-//        window.display();
-//    }
-//}
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
 
 //// comentaar om te testen of dingen gecommit worden vanout github desktop
 
 
-#include <glfw3.h>
-
-int main(void)
-{
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
-}
+//#include <glfw3.h>
+//
+//int main(void)
+//{
+//    GLFWwindow* window;
+//
+//    /* Initialize the library */
+//    if (!glfwInit())
+//        return -1;
+//
+//    /* Create a windowed mode window and its OpenGL context */
+//    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//    if (!window)
+//    {
+//        glfwTerminate();
+//        return -1;
+//    }
+//
+//    /* Make the window's context current */
+//    glfwMakeContextCurrent(window);
+//
+//    /* Loop until the user closes the window */
+//    while (!glfwWindowShouldClose(window))
+//    {
+//        /* Render here */
+//        glClear(GL_COLOR_BUFFER_BIT);
+//
+//        /* Swap front and back buffers */
+//        glfwSwapBuffers(window);
+//
+//        /* Poll for and process events */
+//        glfwPollEvents();
+//    }
+//
+//    glfwTerminate();
+//    return 0;
+//}
